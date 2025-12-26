ddc5f4919a0e36e0d7e9050ca839fb820591653e
hfzfjr <hafizfajar0101@gmail.com>
2025-12-26 15:32:37 +0700

    fix error

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ if __name__ == '__main__':
     if not os.path.exists('templates'):
         os.makedirs('templates')
 
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
